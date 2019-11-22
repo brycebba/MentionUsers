81671405123b35d777380eeb69b61e3de1921815
brycebba <brycebba@gmail.com>
2019-11-21 16:51:38 -0800

    lots of updates but mostly done

--- MentionUsers/locale/ossn.en.php
+++
@@ -1,6 +0,0 @@
-<?php
- $en = array(
-	'ossn:notifications:post:created' => '%s mentioned you in a post!',
-	'ossn:notifications:created' => '%s mentioned you in a comment!'		
-);
-ossn_register_languages('en', $en); 
